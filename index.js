126f6a113fc88ee0744036fdb74716cdd6609894
julong <hahagjl@163.com>
2024-04-08 17:20:13 +0800

    -m

---
+++ index.js
@@ -0,0 +1,20 @@
+const cursorFollwer=document.getElementById("cursor-follower")
+
+document.addEventListener('mousemove', function(event) {
+  var width = window.innerWidth;  
+  var height = window.innerHeight;  
+  var originX = width/2;
+  var originY = height/2;
+  var x = event.clientX;
+  var y = event.clientY;
+  var transY=-(x-originX)/originX;
+  var transX=(y-originY)/originY;
+  cursorFollwer.style.transform = "perspective(1000px)"+ "rotateX("+transX*5+"deg) "+"rotateY("+transY*5+"deg)" +" scale3d(1, 1, 1) ";
+});
+
+
+
+
+
+
+
