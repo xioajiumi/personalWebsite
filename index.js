27460fa7ed859990d2b015678dad3bfdd3bd3cf7
julong <hahagjl@163.com>
2024-08-24 22:32:17 +0800

    备案

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const cursorFollwer = document.getElementById("cursor-follower");
 
 document.addEventListener("mousemove", function (event) {
-  var width = window.innerWidth;
-  var height = window.innerHeight;
-  var originX = width / 2;
-  var originY = height / 2;
-  var x = event.clientX;
-  var y = event.clientY;
-  var transY = -(x - originX) / originX;
-  var transX = (y - originY) / originY;
-  cursorFollwer.style.transform =
-    "perspective(1000px)" +
-    "rotateX(" +
-    transX * 5 +
-    "deg) " +
-    "rotateY(" +
-    transY * 5 +
-    "deg)" +
-    " scale3d(1, 1, 1) ";
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let originX = width / 2;
+  let originY = height / 2;
+  let x = event.clientX;
+  let y = event.clientY;
+  let transY = -(x - originX) / originX;
+  let transX = (y - originY) / originY;
+  const dangerRange=1
+  if((y<=dangerRange)||(height-y<=dangerRange)){
+      cursorFollwer.style.transform =
+      "perspective(1000px)" +
+      "rotateX(" +
+      transX * 0 +
+      "deg) " +
+      "rotateY(" +
+      transY * 0 +
+      "deg)" +
+      " scale3d(1, 1, 1) ";
+  
+  }else{
+    setTimeout(function rotate() {
+      cursorFollwer.style.transform =
+      "perspective(1000px)" +
+      "rotateX(" +
+      transX * 5 +
+      "deg) " +
+      "rotateY(" +
+      transY * 5 +
+      "deg)" +
+      " scale3d(1, 1, 1) ";
+    },50)
+  }
+
+
+
 });
-
-//萨达萨达是
-let a = function () {};
-
-
-
 
 
 
