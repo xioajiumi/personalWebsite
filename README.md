9f447e1e2113b0514ee38721dfc942b7e5d311ea
julong <hahagjl@163.com>
2024-09-10 08:35:14 +0800

    ADD:重做-基本完成

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # personalWebsite
 it's the soure code for my personnal website, welcome to visit it http://giantdragon.xyz/
+
+
+----未来计划
+改进：鼠标离开后旋转不会平滑回弹
+
+改进：移动端lazy-loading
+
+新增：黑色层增加微透明从右到左的头像大背景
+
+改进：滚动条丑
+
+改进:移动端图悬浮缩放
+
+
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+//在这里处理界面跟随鼠标摇晃
 const cursorFollwer = document.getElementById("cursor-follower");
-
 document.addEventListener("mousemove", function (event) {
   let width = window.innerWidth;
   let height = window.innerHeight;
@@ -9,9 +9,9 @@ document.addEventListener("mousemove", function (event) {
   let y = event.clientY;
   let transY = -(x - originX) / originX;
   let transX = (y - originY) / originY;
-  const dangerRange=1
-  if((y<=dangerRange)||(height-y<=dangerRange)){
-      cursorFollwer.style.transform =
+  const dangerRange = 1;
+  if (y <= dangerRange || height - y <= dangerRange) {
+    cursorFollwer.style.transform =
       "perspective(1000px)" +
       "rotateX(" +
       transX * 0 +
@@ -20,28 +20,25 @@ document.addEventListener("mousemove", function (event) {
       transY * 0 +
       "deg)" +
       " scale3d(1, 1, 1) ";
-  
-  }else{
+  } else {
     setTimeout(function rotate() {
       cursorFollwer.style.transform =
-      "perspective(1000px)" +
-      "rotateX(" +
-      transX * 5 +
-      "deg) " +
-      "rotateY(" +
-      transY * 5 +
-      "deg)" +
-      " scale3d(1, 1, 1) ";
-    },50)
+        "perspective(1000px)" +
+        "rotateX(" +
+        transX * 5 +
+        "deg) " +
+        "rotateY(" +
+        transY * 5 +
+        "deg)" +
+        " scale3d(1, 1, 1) ";
+    }, 50);
   }
-
-
-
 });
 
-
-
-
-
-
-
+//console输出的话
+const consoleGreets=[
+  "既然你都打开了开发者工具，想必也是懂的",
+  "Github传送门=> https://github.com/xioajiumi",
+  "虽然也没有啥好玩的 ,,ԾㅂԾ,,",
+]
+consoleGreets.forEach(greet=>console.error(greet))
